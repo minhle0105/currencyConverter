ecac373c5f8cdc7fd8442d0eb76658f62221922b
Minh Le <minh.anh.le.cnn@gmail.com>
2020-12-08 15:31:56 +0700

    Upgrade version not using if else

--- currencyConvert.js
+++ currencyConvert.js
@@ -1,21 +1,6 @@
 function convert() {
-     let amount = document.getElementById("amount").value;
-     let currencyFrom = document.getElementById("currencyFrom").value;
-     let currencyTo = document.getElementById("currencyTo").value;
-     if (currencyFrom == "VND") {
-           if (currencyTo == "VND") {
-                 document.getElementById('result').innerText = "Result: " + amount + " VND.";
-           }
-           else{
-                 document.getElementById('result').innerText = "Result: " + amount/23.160 + " USD.";
-           }
-     }
-      else {
-            if (currencyTo == "USD") {
-                  document.getElementById('result').innerText = "Result: " + amount + " USD."; 
-            }
-            else {
-                  document.getElementById('result').innerText = "Result: " + amount * 23.160 + " VND."
-            }
-      } 
+      let amount = document.getElementById("amount").value;
+      let currencyFrom = document.getElementById("currencyFrom").value;
+      let currencyTo = document.getElementById("currencyTo").value;
+      document.getElementById('result').innerText = "Result: " + (amount*currencyFrom)/currencyTo;
 }
